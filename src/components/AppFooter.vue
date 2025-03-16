a86409bdb6b4e04f23ae4e9c8147751c37ef7e60
vinerima <vinerima@outlook.com>
2025-03-16 20:37:07 +0100

    adds link to footer and readme

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -1,10 +1,13 @@
 <template>
   <footer>
-    Please note, that I don't have any power over what is displayed here. Be aware that some skeets
-    might be harmful or offensive and even illegal. I can't take any responsibility for it.
-    <a href="https://bsky.app/profile/did:plc:ar7c4by46qjdydhdevvrndac" target="_blank"
-      >If you want to report any skeet please contact the bluesky moderation team.</a
-    >
+    <p>
+      Please note, that I don't have any power over what is displayed here. Be aware that some
+      skeets might be harmful or offensive and even illegal. I can't take any responsibility for it.
+      <a href="https://bsky.app/profile/did:plc:ar7c4by46qjdydhdevvrndac" target="_blank"
+        >If you want to report any skeet please contact the bluesky moderation team.</a
+      >
+    </p>
+    <p>(c) 2025 <a href="https://github.com/vinerima/bsky-realtime" target="_blank">vinerima</a></p>
   </footer>
 </template>
 
